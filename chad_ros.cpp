777452885e81285f1d3489d0ecf69fef5a205d10
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2024-11-19 13:34:42 +0100

    main node

--- chad_ros.cpp
+++ chad_ros.cpp
@@ -1,34 +1,50 @@
-#include <memory>
-#include <rclcpp/rclcpp.hpp>
-#include <std_msgs/msg/string.hpp>
-#include <sensor_msgs/msg/point_cloud2.hpp>
-#include <geometry_msgs/msg/pose_stamped.hpp>
-using std::placeholders::_1;
+#include "chad_ros.hpp"
 
 struct ChadRos: public rclcpp::Node {
     ChadRos(): Node("minimal_subscriber") {
-        _sub_deskewed = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/robot/dlio/odom_node/pointcloud/deskewed", queue_size, std::bind(&ChadRos::callback_deskewed, this, _1));
+        // _sub_deskewed = this->create_subscription<sensor_msgs::msg::PointCloud2>(
+        //     "/robot/dlio/odom_node/pointcloud/deskewed", queue_size, std::bind(&ChadRos::callback_points, this, std::placeholders::_1));
         _sub_keyframe = this->create_subscription<sensor_msgs::msg::PointCloud2>(
-            "/robot/dlio/odom_node/pointcloud/keyframe", queue_size, std::bind(&ChadRos::callback_keyframe, this, _1));
+            "/robot/dlio/odom_node/pointcloud/keyframe", queue_size, std::bind(&ChadRos::callback_points, this, std::placeholders::_1));
         _sub_pose = this->create_subscription<geometry_msgs::msg::PoseStamped>(
-            "/robot/dlio/odom_node/pose", queue_size, std::bind(&ChadRos::callback_pose, this, _1));
+            "/robot/dlio/odom_node/pose", queue_size, std::bind(&ChadRos::callback_pose, this, std::placeholders::_1));
     }
 
-    void callback_deskewed(const sensor_msgs::msg::PointCloud2&) {
+    void callback_points(const sensor_msgs::msg::PointCloud2& msg) {
+        // extract pointcloud from message
+        pcl::PointCloud<Point> pointcloud = {};
+        pcl::fromROSMsg(msg, pointcloud);
+        std::vector<Eigen::Vector3f> points;
+        points.reserve(pointcloud.points.size());
+        for (const auto& point: pointcloud.points) {
+            points.push_back({point.x, point.y, point.z});
+        }
+
+        // insert points into TSDF CHAD
         // TODO
     }
-    void callback_keyframe(const sensor_msgs::msg::PointCloud2&) {
-        // TODO
-    }
-    void callback_pose(const geometry_msgs::msg::PoseStamped&) {
-        // TODO
+    void callback_pose(const geometry_msgs::msg::PoseStamped& msg) {
+        // extract position
+        _cur_pos = {
+            (float)msg.pose.position.x,
+            (float)msg.pose.position.y,
+            (float)msg.pose.position.z
+        };
+        // extract rotation
+        _cur_rot = {
+            (float)msg.pose.orientation.w,
+            (float)msg.pose.orientation.x,
+            (float)msg.pose.orientation.y,
+            (float)msg.pose.orientation.z
+        };
     }
 
+    uint32_t queue_size = 10;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _sub_deskewed;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _sub_keyframe;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr _sub_pose;
-    uint32_t queue_size = 10;
+    Eigen::Vector3f _cur_pos = { 0, 0, 0 };
+    Eigen::Quaternionf _cur_rot = { 1, 0, 0, 0 };
 };
 
 int main(int argc, char * argv[]) {
---
+++ chad_ros.hpp
@@ -0,0 +1,34 @@
+#pragma once
+#include <cstdio>
+// ROS2
+#include <rclcpp/rclcpp.hpp>
+#include <std_msgs/msg/string.hpp>
+#include <sensor_msgs/msg/point_cloud2.hpp>
+#include <geometry_msgs/msg/pose_stamped.hpp>
+#include <pcl/point_cloud.h>
+#include <pcl/point_types.h>
+#include <pcl_conversions/pcl_conversions.h>
+// ext
+#include <Eigen/Eigen>
+
+struct Point {
+    Point(): data{0.f, 0.f, 0.f, 1.f} {}
+
+    PCL_ADD_POINT4D;
+    float intensity; // intensity
+    union {
+        std::uint32_t t; // time since beginning of scan in nanoseconds
+        float time; // time since beginning of scan in seconds
+        double timestamp; // absolute timestamp in seconds
+    };
+    EIGEN_MAKE_ALIGNED_OPERATOR_NEW
+}
+EIGEN_ALIGN16;
+POINT_CLOUD_REGISTER_POINT_STRUCT(Point,
+    (float, x, x)
+    (float, y, y)
+    (float, z, z)
+    (float, intensity, intensity)
+    (std::uint32_t, t, t)
+    (float, time, time)
+    (double, timestamp, timestamp))
