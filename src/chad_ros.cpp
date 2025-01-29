3807984428ddae40729e0dded12ee9b3515f6a5f
Jan Kuhlmann <33833587+M2-TE@users.noreply.github.com>
2025-01-29 10:54:03 +0100

    benchmark prep

--- src/chad_ros.cpp
+++ src/chad_ros.cpp
@@ -8,6 +8,7 @@ struct ChadRos: public rclcpp::Node {
         _sub_deskewed = this->create_subscription<sensor_msgs::msg::PointCloud2>(pointcloud_topic, queue_size, std::bind(&ChadRos::callback_points, this, std::placeholders::_1));
         _sub_pose = this->create_subscription<geometry_msgs::msg::PoseStamped>(pose_topic, queue_size, std::bind(&ChadRos::callback_pose, this, std::placeholders::_1));
 
+        // init mapping backends
         float voxel_resolution = VOXEL_RESOLUTION;
         float sdf_truncation = VOXEL_RESOLUTION * 2;
         bool space_carving = false;
@@ -17,8 +18,20 @@ struct ChadRos: public rclcpp::Node {
         #elif MAPPING_BACKEND == 2
             nvblox_mapper_p = new nvblox::Mapper(voxel_resolution, nvblox::MemoryType::kDevice);
         #endif
+
+        // create a new measurement file
+        #ifdef BENCHMARKING
+            measurements = std::ofstream{ "measurements.csv" };
+            measurements << "frame,mem,dur\n";
+        #endif
     }
     ~ChadRos() {
+        #ifdef BENCHMARKING
+            measurements.close();
+        #endif
+
+        return; // DISABLING MAP CONSTUCTION
+
         #if MAPPING_BACKEND == 0
             chad.merge_all_subtrees();
             chad.print_stats();
@@ -34,7 +47,7 @@ struct ChadRos: public rclcpp::Node {
             for (size_t i = 0; i < triangles.size(); i++) {
                 F.row(i) = Eigen::VectorXi::Map(&triangles[i][0], triangles[i].size());
             }
-            std::string filename = "maps/mesh.ply";
+            std::string filename = "mesh.ply";
             igl::write_triangle_mesh(filename, V, F, igl::FileEncoding::Binary);
         #elif MAPPING_BACKEND == 2
             nvblox_mapper_p->saveMeshAsPly("maps/nvblox");
@@ -47,11 +60,17 @@ struct ChadRos: public rclcpp::Node {
         pcl::fromROSMsg(msg, pointcloud);
         std::vector<std::array<float, 3>> points;
         std::vector<Eigen::Vector3d> pointsd;
-        points.reserve(pointcloud.points.size());
-        for (const auto& point: pointcloud.points) {
-            points.push_back({point.x, point.y, point.z});
-            pointsd.push_back({(double)point.x, (double)point.y, (double)point.z});
-        }
+        #if MAPPING_BACKEND == 1
+            pointsd.reserve(pointcloud.points.size());
+            for (const auto& point: pointcloud.points) {
+                pointsd.push_back({(double)point.x, (double)point.y, (double)point.z});
+            }
+        #else
+            points.reserve(pointcloud.points.size());
+            for (const auto& point: pointcloud.points) {
+                points.push_back({point.x, point.y, point.z});
+            }
+        #endif
 
         #ifdef BENCHMARKING
             auto beg = std::chrono::high_resolution_clock::now();
@@ -63,16 +82,13 @@ struct ChadRos: public rclcpp::Node {
             chad.insert(points, _cur_pos, _cur_rot);
         #elif MAPPING_BACKEND == 1 // VDBFusion
             Eigen::Vector3d pos = _cur_pos.cast<double>();
-            vdb_volume_p->Integrate(pointsd, pos, [](float weighting_input) { return weighting_input; });
+            vdb_volume_p->Integrate(pointsd, pos, [](float weighting_input) { return 1.0f; });
         #elif MAPPING_BACKEND == 2 // NVBlox
         #endif
 
         #ifdef BENCHMARKING
             auto end = std::chrono::high_resolution_clock::now();
             std::chrono::milliseconds dur = std::chrono::duration_cast<std::chrono::milliseconds>(end - beg);
-            total += dur;
-            min = std::min(min, dur);
-            max = std::max(max, dur);
 
             // manually reset pointcloud buffers before reading memory
             points = {};
@@ -80,21 +96,18 @@ struct ChadRos: public rclcpp::Node {
             pointcloud = {};
 
             // measure physical memory footprint
+            double mb = (double)read_phys_mem_kb() / 1024.0;
             #if MAPPING_BACKEND == 0
-                double mb = (double)read_phys_mem_kb() / 1024.0;
-                double mb_read = chad.get_readonly_size();
-                double mb_hash = chad.get_hash_size();
-            #elif MAPPING_BACKEND > 0
-                double mb = (double)read_phys_mem_kb() / 1024.0;
+                // double mb_read = chad.get_readonly_size(); // memory leak?
+                // double mb_hash = chad.get_hash_size(); // memory leak?
             #endif
 
             // write measurements to csv
-            std::ofstream file { "measurements.csv", std::ofstream::app };
-            file << frame_count++ << ',' 
-                << mb << ',' 
+            measurements
+                << frame_count++ << ','
+                << mb << ','
                 << dur.count()
-                << '\n';
-            file.close();
+                << std::endl;
         #endif
     }
     void callback_pose(const geometry_msgs::msg::PoseStamped& msg) {
@@ -113,7 +126,7 @@ struct ChadRos: public rclcpp::Node {
         };
     }
 
-    uint32_t queue_size = 1000;
+    uint32_t queue_size = 100;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _sub_deskewed;
     rclcpp::Subscription<sensor_msgs::msg::PointCloud2>::SharedPtr _sub_keyframe;
     rclcpp::Subscription<geometry_msgs::msg::PoseStamped>::SharedPtr _sub_pose;
@@ -129,9 +142,7 @@ struct ChadRos: public rclcpp::Node {
     #endif
 
     // benchmarking vars
-    std::chrono::milliseconds min = std::chrono::milliseconds::max();
-    std::chrono::milliseconds max = std::chrono::milliseconds::min();
-    std::chrono::milliseconds total = std::chrono::milliseconds::zero();
+    std::ofstream measurements;
     size_t frame_count = 0;
 };
 
